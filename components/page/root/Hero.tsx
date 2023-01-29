70b8ec40747fc045c57fad78c64b7a8f3b0433f0
Alex Keo <alexk9081@gmail.com>
2023-01-29 00:36:26 -0500

    Added text to mobile main hero map nav

--- components/page/root/Hero.tsx
+++ components/page/root/Hero.tsx
@@ -8,20 +8,71 @@ export default function MainHero() {
       <HeroText>Elevate your Productivity</HeroText>
 
       <Link href="/map">
-        <Image
-          src="https://media.discordapp.net/attachments/1067491860286820394/1068360876056387614/CamScanner_01-26-2023_11.59_2.jpg?width=806&height=676"
-          alt=""
-        />
+        <MapHolder>
+          <Image
+            src="https://media.discordapp.net/attachments/1067491860286820394/1068360876056387614/CamScanner_01-26-2023_11.59_2.jpg?width=806&height=676"
+            alt=""
+          />
+          <MapText>Map</MapText>
+        </MapHolder>
       </Link>
     </Hero>
   );
 }
 
+const MapHolder = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  right: 2rem;
+
+  @media (max-width: ${screen.tablet}) {
+    bottom: 0;
+    right: 0;
+  }
+`;
+
+const MapText = styled.div`
+  display: none;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  transform: translate(-50%, -50%);
+
+  font-family:${fonts.serifMain};
+  color: ${colors.nearWhite};
+  font-size: 2rem;
+  font-weight: 800;
+  text-shadow: 0 0 1rem ${colors.nearBlack};
+
+  @media (max-width: ${screen.tablet}) {
+    display: block;
+  }
+`;
+
 const HeroText = styled.div`
+  display: flex;
+  align-items: center;
+
   font-size: 4rem;
   font-weight: 800;
 
   font-family: ${fonts.serifMain};
+
+  flex-grow: 1;
+
+  @media (max-width: ${screen.desktop}) {
+    font-size: 3rem;
+  }
+
+  @media (max-width: ${screen.tablet}) {
+    font-size: 2rem;
+  }
+
+  @media (max-width: ${screen.mobile}) {
+    font-size: 1.5rem;
+  }
 `;
 
 const Hero = styled.div`
@@ -43,10 +94,6 @@ const Hero = styled.div`
 `;
 
 const Image = styled.img`
-  position: absolute;
-  bottom: 1rem;
-  right: 2rem;
-
   height: 175px;
 
   border: 4px solid ${colors.unfBlue};
@@ -60,11 +107,12 @@ const Image = styled.img`
     border: none;
     border-radius: 0;
 
-    bottom: 0;
-    right: 0;
+    box-shadow: 0rem 0rem 1rem #00000080;
 
     width: 100vw;
     height: 200px;
     object-fit: cover;
+
+    filter: brightness(0.5);
   }
 `;
