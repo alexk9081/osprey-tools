953900d9234aff58db04fd6862f3e6674b20850b
Alex Keo <alexk9081@gmail.com>
2023-03-09 01:08:07 -0500

    Separated sections for public and personal sets

--- pages/notecards/index.tsx
+++ pages/notecards/index.tsx
@@ -37,9 +37,9 @@ export default function NoteCardsPage() {
           Put highlighted note card packs here or a call to action to make a new
           notecard pack
         </Hero>
-
-        <NoteCardCollections>
-          {data.map((cardCollection) => (
+        
+        <CardSection title="Personal Packs">
+          {data.slice(1, 2).map((cardCollection: any) => (
             <Pack
               title={cardCollection.title}
               desc={cardCollection.desc}
@@ -59,7 +59,22 @@ export default function NoteCardsPage() {
               name: "Current User",
             }}
           />
-        </NoteCardCollections>
+        </CardSection>
+
+        <SectionBreak />
+
+        <CardSection title="Public Packs">
+          {data.map((cardCollection: any) => (
+            <Pack
+              title={cardCollection.title}
+              desc={cardCollection.desc}
+              img={cardCollection.img}
+              link={cardCollection.link}
+              creator={cardCollection.creator}
+              key={cardCollection.creator.name + cardCollection.title}
+            />
+          ))}
+        </CardSection>
       </main>
     </>
   );
@@ -73,11 +88,35 @@ const Hero = styled.div`
   align-items: center;
 `;
 
+const SectionBreak = styled.hr`
+  margin: 0 4rem;
+`;
+
+function CardSection({ title, children }: { title: string; children: any }) {
+  return (
+    <CardSectionWrapper>
+      <SectionTitle>{title}</SectionTitle>
+      <NoteCardCollections>{children}</NoteCardCollections>
+    </CardSectionWrapper>
+  );
+}
+
+const CardSectionWrapper = styled.div`
+  margin: 4rem 2rem;
+`;
+
+const SectionTitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: 700;
+
+  margin-bottom: 1rem;
+`;
+
 const NoteCardCollections = styled.div`
   display: flex;
   gap: 0.75rem;
   flex-direction: row;
   flex-wrap: wrap;
 
-  margin: 4rem 2rem;
+  margin-bottom: 2rem;
 `;
