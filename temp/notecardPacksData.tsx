6b8ffb4e3064833f1b823248a8cabb9516436edb
Alex Keo <alexk9081@gmail.com>
2023-04-12 19:40:34 -0400

    Exported pack data

---
+++ temp/notecardPacksData.tsx
@@ -0,0 +1,24 @@
+const data = [
+  {
+    title: "Software Engineering",
+    desc: "Study scrum concepts, unit testing, and how to break production",
+    img: "https://images.unsplash.com/photo-1580894908361-967195033215?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    link: "/notecards/packs/Wall-E/softwareEngineering",
+    creator: {
+      img: "https://images.unsplash.com/photo-1589254065878-42c9da997008?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      name: "Wall-E",
+    },
+  },
+  {
+    title: "Construction of Language Transistors",
+    desc: "Figure out how to make a compiler on your own",
+    img: "https://images.unsplash.com/photo-1629706167922-f7d29bb50450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    link: "/notecards/packs/Gradma/languageTransistors",
+    creator: {
+      img: "https://images.unsplash.com/photo-1442458370899-ae20e367c5d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+      name: "Gradma",
+    },
+  },
+];
+
+export default data;
