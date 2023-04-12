cb11e47ac7406ba4347cb973b0112918bbb181b7
Alex Keo <alexk9081@gmail.com>
2023-04-12 19:33:06 -0400

    Addded page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
 import EventsProvider from "@/components/layout/CalendarContext";
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+
   return (
     <>
       <Layout>
         <EventsProvider>
-          <Component {...pageProps} />
+          {getLayout(<Component {...pageProps} />)}
         </EventsProvider>
       </Layout>
     </>
