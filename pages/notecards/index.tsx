19c9d8f04868292eb3ec594cdb44e680f598992f
Alex Keo <alexk9081@gmail.com>
2023-04-13 01:47:35 -0400

    Restyled notecards page

--- pages/notecards/index.tsx
+++ pages/notecards/index.tsx
@@ -1,8 +1,15 @@
+import CardSection from "@/components/page/notecards/CardSection";
 import Hero from "@/components/page/notecards/Hero";
 import Pack from "@/components/page/notecards/Pack";
 import data from "@/temp/notecardPacksData";
 import Head from "next/head";
 import styled from "styled-components";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { screen } from "@/styles/styleConstants";
 
 export default function NoteCardsPage() {
   return (
@@ -12,79 +19,167 @@ export default function NoteCardsPage() {
       </Head>
       <main>
         <Hero />
-        
-        <CardSection title="Your Notecards">
-          {data.slice(1, 2).map((cardCollection: any) => (
-            <Pack
-              title={cardCollection.title}
-              desc={cardCollection.desc}
-              img={cardCollection.img}
-              link={cardCollection.link}
-              creator={cardCollection.creator}
-              key={cardCollection.creator.name + cardCollection.title}
-            />
-          ))}
-          <Pack
-            title="Add new collection"
-            desc="Create your own note card collection"
-            img="https://cdn-icons-png.flaticon.com/512/32/32339.png"
-            link="/notecards/create"
-            creator={{
-              img: "https://genslerzudansdentistry.com/wp-content/uploads/2015/11/anonymous-user.png",
-              name: "Current User",
+
+        <CardSection
+          title="Your Notecards"
+          description="Your own personally made notecards"
+        >
+          <StyledSwiper
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{ clickable: true }}
+            breakpoints={{
+              480: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 0,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              1500: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
             }}
-          />
+          >
+            {data.slice(1, 2).map((cardCollection: any) => (
+              <SwiperSlide
+                key={cardCollection.creator.name + cardCollection.title}
+              >
+                <Pack
+                  title={cardCollection.title}
+                  desc={cardCollection.desc}
+                  img={cardCollection.img}
+                  link={cardCollection.link + "/overview"}
+                  creator={cardCollection.creator}
+                  key={cardCollection.creator.name + cardCollection.title}
+                />
+              </SwiperSlide>
+            ))}
+            <SwiperSlide>
+              <Pack
+                title="Add new collection"
+                desc="Create your own note card collection"
+                img="https://cdn-icons-png.flaticon.com/512/32/32339.png"
+                link="/notecards/create"
+                creator={{
+                  img: "https://genslerzudansdentistry.com/wp-content/uploads/2015/11/anonymous-user.png",
+                  name: "Current User",
+                }}
+              />
+            </SwiperSlide>
+          </StyledSwiper>
         </CardSection>
 
+        <HorizontalRule />
 
-        <CardSection title="Public Notecards">
-          {data.map((cardCollection: any) => (
-            <Pack
-              title={cardCollection.title}
-              desc={cardCollection.desc}
-              img={cardCollection.img}
-              link={cardCollection.link}
-              creator={cardCollection.creator}
-              key={cardCollection.creator.name + cardCollection.title}
-            />
-          ))}
+        <CardSection
+          title="Public Notecards"
+          description="Community shared notecards"
+        >
+          <StyledSwiper
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{ clickable: true }}
+            breakpoints={{
+              480: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 0,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              1500: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }}
+          >
+            {data.map((cardCollection: any) => (
+              <SwiperSlide
+                key={cardCollection.creator.name + cardCollection.title}
+              >
+                <Pack
+                  title={cardCollection.title}
+                  desc={cardCollection.desc}
+                  img={cardCollection.img}
+                  link={cardCollection.link + "/overview"}
+                  creator={cardCollection.creator}
+                  key={cardCollection.creator.name + cardCollection.title}
+                />
+              </SwiperSlide>
+            ))}
+            {data.map((cardCollection: any) => (
+              <SwiperSlide
+                key={cardCollection.creator.name + cardCollection.title}
+              >
+                <Pack
+                  title={cardCollection.title}
+                  desc={cardCollection.desc}
+                  img={cardCollection.img}
+                  link={cardCollection.link + "/overview"}
+                  creator={cardCollection.creator}
+                  key={cardCollection.creator.name + cardCollection.title}
+                />
+              </SwiperSlide>
+            ))}
+            {data.map((cardCollection: any) => (
+              <SwiperSlide
+                key={cardCollection.creator.name + cardCollection.title}
+              >
+                <Pack
+                  title={cardCollection.title}
+                  desc={cardCollection.desc}
+                  img={cardCollection.img}
+                  link={cardCollection.link + "/overview"}
+                  creator={cardCollection.creator}
+                  key={cardCollection.creator.name + cardCollection.title}
+                />
+              </SwiperSlide>
+            ))}
+            {data.map((cardCollection: any) => (
+              <SwiperSlide
+                key={cardCollection.creator.name + cardCollection.title}
+              >
+                <Pack
+                  title={cardCollection.title}
+                  desc={cardCollection.desc}
+                  img={cardCollection.img}
+                  link={cardCollection.link + "/overview"}
+                  creator={cardCollection.creator}
+                  key={cardCollection.creator.name + cardCollection.title}
+                />
+              </SwiperSlide>
+            ))}
+          </StyledSwiper>
         </CardSection>
       </main>
     </>
   );
 }
 
-function CardSection({ title, children }: { title: string; children: any }) {
-  return (
-    <SectionSet>
-      <SectionName>
-        {title}
-        </SectionName>
-    <NoteCardCollections>{children}</NoteCardCollections>
-    </SectionSet>
-  );
-}
+const StyledSwiper = styled(Swiper)`
+  @media (max-width: ${screen.tablet}) {
+    .swiper-button-prev {
+      display: none;
+    }
 
-const SectionSet = styled.fieldset`
-  margin: 3rem 2rem;
-  padding: 1rem;
-
-  border-radius: 2rem;
+    .swiper-button-next {
+      display: none;
+    }
+  }
 `;
 
-const SectionName = styled.legend`
-  font-weight: 600;
-  font-size: 1.5rem;
-
-  margin-left: 1rem;
-  padding: 0 0.5rem;
-`;
-
-const NoteCardCollections = styled.div`
-  display: flex;
-  gap: 0.75rem;
-  flex-direction: row;
-  flex-wrap: wrap;
-
-  margin-bottom: 2rem;
+const HorizontalRule = styled.hr`
+  margin: 0 2rem;
 `;
