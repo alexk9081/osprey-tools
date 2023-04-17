041d3771e3183ea9d3e3580e2158a3779f437bde
Alex Keo <alexk9081@gmail.com>
2023-04-17 00:19:28 -0400

    Changed mobile quick location width

--- components/page/root/PopularLocations.tsx
+++ components/page/root/PopularLocations.tsx
@@ -52,6 +52,10 @@ const Locations = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
+
+  @media (max-width: ${screen.tablet}) {
+    margin: 2rem 0rem;
+  }
 `;
 
 const FavoriteLocationsWrapper = styled.div`
