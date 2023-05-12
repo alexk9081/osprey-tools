822ee88f04f969a61e25953c24f9111d29dfba6b
Alex Keo <alexk9081@gmail.com>
2023-05-11 20:05:28 -0400

    Added loading icon for route protector

--- components/page/users/RouteProtector.tsx
+++ components/page/users/RouteProtector.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import { useContext, useEffect } from "react";
 import Head from "next/head";
 import { UserAuthContext } from "@/components/layout/UserAuthContext";
+import styled, { keyframes } from "styled-components";
+import { colors } from "@/styles/styleConstants";
 
 export default function RouteProtector({
   pageName,
@@ -25,9 +27,15 @@ export default function RouteProtector({
 
   if (isInverse ? !userAuth : userAuth) {
     return (
-      <Head>
-        <title>{pageName}</title>
-      </Head>
+      <>
+        <Head>
+          <title>{pageName}</title>
+        </Head>
+        <MainContent>
+          <LoadingIcon />
+          <LoadingMessage>Loading...</LoadingMessage>
+        </MainContent>
+      </>
     );
   } else {
     return (
@@ -40,3 +48,35 @@ export default function RouteProtector({
     );
   }
 }
+
+const MainContent = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoadingIcon = styled.div`
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid ${colors.unfBlueLight}; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+
+  animation: ${spin} 2s linear infinite;
+`;
+
+const LoadingMessage = styled.div`
+  margin-top: 1.5rem;
+  font-weight: 600;
+  font-size: 2rem;
+`;
